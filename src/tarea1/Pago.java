2cbdfa439978fa6940d023c09c4ad3bbaf4d14cf
Rambi <josue290x31@gmail.com>
2022-10-08 13:59:27 -0300

    agregue pago completo, trabajando en docTributario

--- src/tarea1/Pago.java
+++ src/tarea1/Pago.java
@@ -15,7 +15,11 @@ public class Pago {
     }
     protected Date GetFecha(){
         return fecha;
-    }   
+    }
+    @Override
+    public String toString(){
+        return "monto:" + monto + "fecha:" + fecha;
+    } 
 }
 
 
@@ -33,4 +37,55 @@ class Efectivo extends Pago{
           return dinero - Getmonto();
         }
     }
+}
+
+class Transferencia extends Pago{
+    private String banco;
+    private String numCuenta;
+    public Transferencia(float Getmonto, String banco, String numCuenta){
+        super(Getmonto);
+        this.banco = banco;
+        this.numCuenta = numCuenta;
+    }
+    public String Getbanco(){
+        return banco;
+    }
+    
+    public String GetnumCuenta(){
+        return numCuenta;
+    }
+    @Override
+    public String toString(){
+        return "monto:" + Getmonto() + "banco:" + banco + "numCuenta:" + numCuenta;
+    } 
+    
+}
+
+
+
+class Tarjeta extends Pago{
+    private String tipo;
+    private String numTransaccion;
+    public Tarjeta(float Getmonto, String tipo, String numTransaccion){
+        super(Getmonto);
+        this.tipo = tipo;
+        this.numTransaccion = numTransaccion;
+    }
+    public String Gettipo(){
+        if (tipo == "credito") {
+            return "credito";
+        } else if (tipo == "debito"){
+            return "debito";
+        } else {
+            return "tarjeta no valida";
+        }
+    }
+    
+    public String GetnumTransaccion(){
+        return numTransaccion;
+    }
+    @Override
+    public String toString(){
+        return "monto:" + Getmonto() + "tipo de tarjeta:" +  tipo + "numTransaccion:" + numTransaccion;
+    } 
 }
