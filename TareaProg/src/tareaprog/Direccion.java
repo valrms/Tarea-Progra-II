bd1c5c7b8d22e25017b4bd44277693bd14d7f071
Rambistik <114706862+Rambistik@users.noreply.github.com>
2022-10-09 22:11:29 -0300

    Update Direccion.java

--- TareaProg/src/tareaprog/Direccion.java
+++ TareaProg/src/tareaprog/Direccion.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tareaprog;
 
-/**
- *
- * @author valeriacarolinaramos
- */
-public class Direccion {
+class Direccion {
+    private String direccion;
+    public Direccion(String direccion){
+        this.direccion = direccion;
+    }
     
+    public String getDireccion() {
+        return direccion;
+    }
 }
