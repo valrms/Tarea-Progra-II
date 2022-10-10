fa0124341787473056e936774cdbd4c0fdd3d754
Rambistik <114706862+Rambistik@users.noreply.github.com>
2022-10-09 22:13:24 -0300

    Update Pago.java

--- TareaProg/src/tareaprog/Pago.java
+++ TareaProg/src/tareaprog/Pago.java
@@ -1,13 +1,88 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tareaprog;
 
-/**
- *
- * @author valeriacarolinaramos
- */
+import java.util.Date;
+
+
 public class Pago {
+    private float monto;
+    private Date fecha;
+    public Pago(float monto){
+        this.monto = monto;
+        fecha = new Date();
+    }
+    protected float Getmonto(){
+        return monto;
+    }
+    protected Date GetFecha(){
+        return fecha;
+    }
+    @Override
+    public String toString(){
+        return "monto:" + monto + "fecha:" + fecha;
+    } 
+}
+
+
+
+class Efectivo extends Pago{
+    public Efectivo(float monto){
+        super(monto);
+    }
+    public float calcDevolucion(float dinero){
+        if (Getmonto() == dinero) {
+            return 0;  
+        } else if (Getmonto() > dinero){
+           return -1;
+        } else {         
+          return dinero - Getmonto();
+        }
+    }
+}
+
+class Transferencia extends Pago{
+    private String banco;
+    private String numCuenta;
+    public Transferencia(float monto, String banco, String numCuenta){
+        super(monto);
+        this.banco = banco;
+        this.numCuenta = numCuenta;
+    }
+    public String Getbanco(){
+        return banco;
+    }
+    public String GetnumCuenta(){
+        return numCuenta;
+    }
+    @Override
+    public String toString(){
+        return "monto:" + Getmonto() + "banco:" + banco + "numCuenta:" + numCuenta;
+    } 
     
+}
+
+class Tarjeta extends Pago{
+    private String tipo;
+    private String numTransaccion;
+    public Tarjeta(float monto, String tipo, String numTransaccion){
+        super(monto);
+        this.tipo = tipo;
+        this.numTransaccion = numTransaccion;
+    }
+    public String Gettipo(){
+        if ("credito".equals(tipo)) {
+            return "credito";
+        } else if ("debito".equals(tipo)){
+            return "debito";
+        } else {
+            return "tarjeta no valida";
+        }
+    }
+    
+    public String GetnumTransaccion(){
+        return numTransaccion;
+    }
+    @Override
+    public String toString(){
+        return "monto:" + Getmonto() + "tipo de tarjeta:" +  tipo + "numTransaccion:" + numTransaccion;
+    } 
 }
