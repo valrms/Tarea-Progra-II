92c18c489551d0fc6c67089964f4b22f20040996
Val Ramos <74368474+valrms@users.noreply.github.com>
2022-10-09 22:37:29 -0300

    Update TareaProg.java
    Working on Main

--- TareaProg/src/tareaprog/TareaProg.java
+++ TareaProg/src/tareaprog/TareaProg.java
@@ -4,7 +4,12 @@ package tareaprog;
 public class TareaProg {
 
     public static void main(String[] args) {
-        System.out.println("Hola");
+        Articulo articulo = new Articulo(5, "silla", "marron", 80000);
+        DetalleCompra details = new DetalleCompra (4, articulo);
+        details.adding(5, articulo);
+        OrdenCompra orders = new OrdenCompra(details);
+        float valor = orders.calcPrecioSinIVA();
+        System.out.println(valor);
     }
     
 }
